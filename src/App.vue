f120331993b4449c58ea3e6c55b0713d38125cbe
tal7aouy <talhaouycoding@gmail.com>
2021-10-27 20:11:13 +0100

    add index and diff for slider

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,21 @@
       <div
         v-for="(slide, index) in 6"
         :key="index"
-        class="absolute mx-2 rounded-lg bg-red-400"
+        class="
+          absolute
+          mx-2
+          rounded-lg
+          bg-red-400
+          transition-transform
+          duration-200
+          ease-in
+        "
         style="width: 350px; height: 580px"
         :style="
           index == indexSelected
-            ? `transform: translate(${380 * index}px) scale(0.9)`
+            ? `transform: translate(${380 * (index + difference)}px) scale(0.9)`
             : `transform: translate(${
-                280 * index
+                280 * (index + difference)
               }px) scale(0.4);cursor:pointer;`
         "
         @click="index != indexSelected ? selectSlide(index) : ''"
@@ -27,9 +35,16 @@ export default {
   name: 'App',
   setup() {
     const indexSelected = ref(0)
-
-    const selectSlide = (index) => (indexSelected.value = index)
-    return { indexSelected, selectSlide }
+    const difference = ref(0)
+    const selectSlide = (index) => {
+      difference.value += indexSelected.value - index
+      indexSelected.value = index
+    }
+    return {
+      difference,
+      indexSelected,
+      selectSlide,
+    }
   },
 }
 </script>
